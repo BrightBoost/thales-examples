6aad8ff0c7c4b1213be3b2ae5d3d83cf92bec95b
brightboost <info@brightboost.nl>
2022-12-14 12:10:12 +0100

    more examples and exercises

---
+++ session1/example_dict.py
@@ -0,0 +1,5 @@
+person = {"name": "John Doe", "age": 30, "gender": "male"}
+print(person["name"])  # Output: "John Doe"
+
+for x in person.items():
+    print(x)
---
+++ session1/example_set.py
@@ -0,0 +1,9 @@
+fruits = {"apple", "banana", "cherry"}
+print(fruits)  # Output: {"apple", "banana", "cherry"}
+
+fruits.add("apple")
+print(fruits)
+
+colors = {"red", "green", "blue"}
+for color in colors:
+    print(color)
