32d42fb0490542d03c73f4fb106166794c6f42e7
brightboost <info@brightboost.nl>
2022-12-20 15:39:26 +0100

    pytest

--- session3/example_pytest.py
+++ session3/example_pytest.py
@@ -5,4 +5,11 @@ def test_example():
 
 def test_another_example():
     # Test code goes here
+    # why we need approx:
+    # x = 0.01
+    # for i in range(0, 20):
+    #     x += 0.01
+    #     print(x)
     assert 1 + 1 == 2
+
+test_another_example()
